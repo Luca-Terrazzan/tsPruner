9b4677dea1d5b729ad61b8a80efe3641be848866
Luca Terrazzan <luca.terraz@gmail.com>
2019-06-20 17:35:29 +0200

    Added a simple file opener class

---
+++ src/files-manipulation/folder-finder.ts
@@ -0,0 +1,17 @@
+import { readdirSync } from 'fs';
+
+export class FolderFinder {
+
+  public openFolder(folderPath: string): string[] {
+    // Contains a list of filenames contained in the folder
+    let folderContent: string[];
+    try {
+      folderContent = readdirSync(folderPath);
+    } catch (e) {
+      console.log('error while reading file:', e);
+    }
+
+    return [];
+  }
+
+}
