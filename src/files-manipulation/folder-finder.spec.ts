d7329354ebbc73355b33d7acee2084b7101e8824
Luca Terrazzan <luca.terraz@gmail.com>
2019-06-20 18:15:12 +0200

    test(folderfinder): more comprehensive tests

--- src/files-manipulation/folder-finder.spec.ts
+++ src/files-manipulation/folder-finder.spec.ts
@@ -12,13 +12,14 @@ describe('Folder finder', () => {
     outputFileSync(`${fixtureBasePath}/test.txt`, 'data');
     outputFileSync(`${fixtureBasePath}/test1.txt`, 'data');
     outputFileSync(`${fixtureBasePath}/test2.txt`, 'data');
+    outputFileSync(`${fixtureBasePath}/inner/testK.txt`, 'data');
   });
 
   it('Should read a folder content', () => {
     const ff: FolderFinder = new FolderFinder();
     const fileList: string[] = ff.openFolder(fixtureBasePath);
 
-    const expectedResult = ['test.txt', 'test1.txt', 'test2.txt'];
+    const expectedResult = ['inner', 'test.txt', 'test1.txt', 'test2.txt'];
 
     expect(fileList).toEqual(expectedResult);
   });
