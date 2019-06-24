69e2cc727c5537688b21b59a72bbdfc249f654a3
Luca Terrazzan <luca.terraz@gmail.com>
2019-06-24 11:01:40 +0200

    feat(metadata): added type validation

--- src/folder-metadata/pruner-metadata.ts
+++ src/folder-metadata/pruner-metadata.ts
@@ -1,4 +1,4 @@
-import { FolderMetadata } from './folder-metadata.type';
+import { FolderMetadata, FileMetadata } from './folder-metadata.type';
 import { readJsonSync } from 'fs-extra';
 import { FolderFinder } from '../files-manipulation/folder-finder';
 import { MetadataNotFoundException, InvalidMetadataException } from './exceptions';
@@ -39,7 +39,32 @@ export class PrunerMetadata {
       this.logger.error('Metadata file not found.');
       throw new MetadataNotFoundException();
     }
+    this.validateMetadataFile(folderMetadata);
 
     return folderMetadata;
   }
+
+  private validateMetadataFile(metadata: FolderMetadata): void {
+    if (!this.determineIfIsValidMetadataFormat(metadata)) {
+      throw new InvalidMetadataException('Invalid metadata format.');
+    }
+  }
+
+  private determineIfIsValidMetadataFormat(metadata: FolderMetadata): metadata is FolderMetadata {
+    if (!metadata.timestamp || !metadata.files) {
+      return false;
+    }
+    for (const fileMetadata of metadata.files) {
+      if (!this.validateSingleFileMetadata(fileMetadata)) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  private validateSingleFileMetadata(metadata: FileMetadata): metadata is FileMetadata {
+    return !(!metadata.fileName || !metadata.timestamp);
+  }
+
 }
