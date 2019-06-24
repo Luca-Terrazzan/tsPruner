22c80b4ee19d0b890fb38e078215d77708b01e74
Luca Terrazzan <luca.terraz@gmail.com>
2019-06-24 10:26:14 +0200

    fix(indent)

--- src/folder-metadata/pruner-metadata.ts
+++ src/folder-metadata/pruner-metadata.ts
@@ -2,35 +2,44 @@ import { FolderMetadata } from './folder-metadata.type';
 import { readJsonSync } from 'fs-extra';
 import { FolderFinder } from '../files-manipulation/folder-finder';
 import { MetadataNotFoundException, InvalidMetadataException } from './exceptions';
+import { Logger } from '../logging/logger';
 
 export const metadataFileName: string = 'prune-metadata.json';
 
 export class PrunerMetadata {
 
-    private readonly folderPath: string;
-    private readonly folderMetadata: FolderMetadata;
+  private readonly folderPath: string;
+  private readonly folderMetadata: FolderMetadata;
 
-    constructor(
-        folderPath: string,
-        private readonly ffinder: FolderFinder
-    ) {
-        this.folderPath = folderPath;
+  constructor(
+    folderPath: string,
+    private readonly ffinder: FolderFinder,
+    private readonly logger: Logger
+  ) {
+    this.folderPath = folderPath;
+  }
+
+  public generateMetadata(filesList: string[]): boolean {
+    return true;
+  }
+
+  public getFolderMetadata(): FolderMetadata {
+    return this.loadRawMetadata();
+  }
+
+  private loadRawMetadata(): FolderMetadata {
+    let folderMetadata: FolderMetadata;
+    try {
+      folderMetadata = readJsonSync(`${this.folderPath}/${metadataFileName}`) as FolderMetadata;
+    } catch (e) {
+      if (e instanceof SyntaxError) {
+        this.logger.error('Invalid metadata file found.');
+        throw new InvalidMetadataException();
+      }
+      this.logger.error('Metadata file not found.');
+      throw new MetadataNotFoundException();
     }
 
-    public generateMetadata(filesList: string[]): boolean {
-        return true;
-    }
-
-    public loadMetadata(): FolderMetadata {
-        try {
-            const folderMetadata: FolderMetadata = readJsonSync(`${this.folderPath}/${metadataFileName}`) as FolderMetadata;
-
-            return folderMetadata;
-        } catch (e) {
-            if (e instanceof SyntaxError) {
-                throw new InvalidMetadataException();
-            }
-            throw new MetadataNotFoundException();
-        }
-    }
+    return folderMetadata;
+  }
 }
