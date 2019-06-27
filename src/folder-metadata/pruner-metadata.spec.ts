f7dce501ab027e583bdf940e9e34e1b040e18e76
Luca Terrazzan <luca.terraz@gmail.com>
2019-06-28 01:10:35 +0200

    test(metadata): added error cases

--- src/folder-metadata/pruner-metadata.spec.ts
+++ src/folder-metadata/pruner-metadata.spec.ts
@@ -3,6 +3,7 @@ import { PrunerMetadata, metadataFileName } from './pruner-metadata';
 import { FolderFinder } from '../files-manipulation/folder-finder';
 import { Logger } from '../logging/logger';
 import { FolderMetadata } from './folder-metadata.type';
+import { MetadataNotFoundException, InvalidMetadataException } from './exceptions';
 
 describe('Pruner metadata', () => {
 
@@ -24,20 +25,37 @@ describe('Pruner metadata', () => {
 
     const expectedFolderMetadata: FolderMetadata = {
       files: [
-        {fileName: 'file1.txt', timestamp: 1000},
-        {fileName: 'file2.txt', timestamp: 1001}
+        { fileName: 'file1.txt', timestamp: 1000 },
+        { fileName: 'file2.txt', timestamp: 1001 }
       ],
       timestamp: 999
     };
 
     expect(metadata).toEqual(expectedFolderMetadata);
+
+    removeSync(fixtureBasePath);
   });
 
-  afterAll(() => {
-    // Remove fixtures
+  it('Should be able handle missing config', () => {
+    const logger = Logger.getInstance();
+    const pm = new PrunerMetadata(`${fixtureBasePath}`, new FolderFinder(logger), logger);
 
-    // Delete test folder
-    removeSync(fixtureBasePath);
+    expect(() => pm.getFolderMetadata()).toThrow(MetadataNotFoundException);
+  });
+
+  it('Should be able handle malformed config', () => {
+    // Create an invalid metadata file
+    outputFileSync(
+      `${fixtureBasePath}/${metadataFileName}`,
+      `{"filesa": [
+        {"fileName": "file1.txt", "timestamp1": 1000},
+        {"fileName2": "file2.txt", "timestampss": 1001}
+        ], "timestaamp": 999}`
+    );
+    const logger = Logger.getInstance();
+    const pm = new PrunerMetadata(`${fixtureBasePath}`, new FolderFinder(logger), logger);
+
+    expect(() => pm.getFolderMetadata()).toThrow(InvalidMetadataException);
   });
 
 });
