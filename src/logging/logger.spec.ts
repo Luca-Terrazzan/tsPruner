67af265d00a1b8665af28bdd36368d61b7e5ea45
Luca Terrazzan <luca.terraz@gmail.com>
2019-07-19 16:56:19 +0200

    chore(logger.test): better coverage

--- src/logging/logger.spec.ts
+++ src/logging/logger.spec.ts
@@ -5,6 +5,9 @@ global.console.log = jest.fn();
 describe('Logger', () => {
 
   it('Should provide logging methods', () => {
+    Logger.info('error test message');
+    Logger.debug('error test message');
+    Logger.warning('error test message');
     Logger.error('error test message');
 
     expect(Logger.info).toBeDefined();
