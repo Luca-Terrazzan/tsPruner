9a19b3a51ec2a2dce335c87250e222310454cfd4
Luca Terrazzan <luca.terraz@gmail.com>
2019-08-12 12:21:50 +0200

    feat(config): added a config parser

---
+++ src/files-manipulation/config.ts
@@ -0,0 +1,24 @@
+import { readJsonSync } from 'fs-extra';
+
+type PrunerConfiguration = {
+  age: number,
+  path: string
+};
+
+const path = './../..';
+
+export class Config {
+  private readonly config: PrunerConfiguration;
+
+  constructor() {
+    this.config = readJsonSync(`${path}/pruner-config.json`) as PrunerConfiguration;
+  }
+
+  public getMaximumAge(): number {
+    return this.config.age;
+  }
+
+  public getPathToPrune(): string {
+    return this.config.path;
+  }
+}
